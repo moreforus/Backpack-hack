c5a83bd4a62c61ccffbd0781fdab99649393eac6
moreforus <jabberwock2march.hare@gmail.com>
2024-08-28 14:27:51 +0300

    diversity (ts5a3159)

--- src/rx5808.cpp
+++ src/rx5808.cpp
@@ -16,7 +16,12 @@ RX5808::Init()
         pinMode(PIN_CS_2, INPUT);
     #endif
 
+#if defined(VIDEO_CTRL)
+    pinMode(VIDEO_CTRL, OUTPUT);
+#endif
+
     DBGLN("RX5808 init complete");
+    //Serial.begin(115200);
 }
 
 void
@@ -25,7 +30,7 @@ RX5808::EnableSPIMode()
     pinMode(PIN_MOSI, OUTPUT);
     pinMode(PIN_CLK, OUTPUT);
     pinMode(PIN_CS, OUTPUT);
-
+    
     digitalWrite(PIN_MOSI, LOW);
     digitalWrite(PIN_CLK, LOW);
     digitalWrite(PIN_CS, HIGH);
@@ -53,4 +58,70 @@ RX5808::SendIndexCmd(uint8_t index)
 
     uint16_t f = frequencyTable[index];
     rtc6715SetFreq(f);
+}
+
+void 
+RX5808::Loop(uint32_t now)
+{
+    ModuleBase::Loop(now);
+#ifdef RSSI_A
+    ANTENNA_TYPE antenna = CheckRSSI(now);
+    if (antenna != currentAntenna)
+    {
+        currentAntenna = antenna;
+        SwitchVideo(currentAntenna);
+    }
+#endif
+}
+
+#define RSSI_FILTER (8)
+#define RSSI_DIFF_BORDER (16)
+
+ANTENNA_TYPE 
+RX5808::CheckRSSI(uint32_t now)
+{
+    static uint8_t filter = RSSI_FILTER;
+    ANTENNA_TYPE antenna = currentAntenna;
+
+    if (now - currentTimeMs < 1)
+    {
+        return antenna;
+    }
+
+    currentTimeMs = now;
+
+    analogRead(RSSI_A);
+    rssiA += analogRead(RSSI_A);
+    
+    analogRead(RSSI_B);
+    rssiB += analogRead(RSSI_B);
+    if (--filter == 0)
+    {
+        rssiA /= RSSI_FILTER;
+        rssiB /= RSSI_FILTER;
+
+        if (rssiA - rssiB > RSSI_DIFF_BORDER)
+        {
+            antenna = ANT_A;
+        }
+        else if (rssiB - rssiA > RSSI_DIFF_BORDER)
+        {
+            antenna = ANT_B;
+        }
+
+        //Serial.printf("%d %d %d", rssiA, rssiB, antenna);
+        //Serial.println();
+
+        rssiA = 0;
+        rssiB = 0;
+        filter = RSSI_FILTER;
+    }
+ 
+    return antenna;
+}
+
+void
+RX5808::SwitchVideo(ANTENNA_TYPE antenna)
+{
+    digitalWrite(VIDEO_CTRL, (antenna == ANT_A ? LOW : HIGH));
 }
--- src/rx5808.h
+++ src/rx5808.h
@@ -4,13 +4,27 @@
 #include <Arduino.h>
 #include <channels.h>
 
+enum ANTENNA_TYPE : uint8_t
+{
+    ANT_A = 0,
+    ANT_B,
+};
+
 class RX5808 : public ModuleBase
 {
 public:
     void Init();
     void SendIndexCmd(uint8_t index);
+    void Loop(uint32_t now);
 
 private:
     void EnableSPIMode();
+    ANTENNA_TYPE CheckRSSI(uint32_t now);
+    void SwitchVideo(ANTENNA_TYPE antenna);
+
     bool SPIModeEnabled = false;
+    ANTENNA_TYPE currentAntenna = ANT_A;
+    uint32_t currentTimeMs = 0;
+    uint16_t rssiA = 0;
+    uint16_t rssiB = 0;
 };
